96b039b88490d17aad99f1981b99555a9fd21a6f
roguespectator17 <78824261+roguespectator17@users.noreply.github.com>
2021-02-12 21:44:37 -0500

    Update characters.py

--- venv/GameFiles/characters.py
+++ venv/GameFiles/characters.py
@@ -1,3 +1,13 @@
 class Characters:
-    def __init__(self):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
         super().__init__()
+
+    def draw(self, surf):
+
+
+    def update(self, dt):
+
+    
