cc40d56c0a19d5d6878778c7b6d06b4bb379f72e
Lane <laneknauff@gmail.com>
2021-02-17 23:34:03 -0500

    Fixed up 7 bugs and fixed the map class
    Still need to add the render but it is pulling all the data and preparing the tiledata in an organized 2D list. should be as easy as pie to read through it. Im done for the night

--- venv/GameFiles/map.py
+++ venv/GameFiles/map.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+import re
 
 class Map:
     """ Map Manager"""
@@ -10,47 +11,50 @@ class Map:
         #self.mTileImageNumY
         #self.mMapWidth # in tiles
         #self.mMapHeight
-        #self.mTileCodes = []
+        self.mTileCodes = []
         self.parseXMl(mapName)
 
 
     def parseXMl(self, fileName):
         try:
-            print("TMXFile\\"+fileName)
-            mapXML = ET.parse("TMXFile\\"+fileName)
-            print("yeet")
+            mapXML = ET.parse("TMXFiles\\"+fileName)
             root = mapXML.getroot()
-            self.mTileWidth = root.attrib("tilewidth")
-            self.mTileHeight = root.attrib("tileheight")
-            self.mMapWidth = root.attrib("width")
-            self.mMapHeight = root.attrib("height")
-            columns
-            tilecount
-            tilesetName
+            self.mTileWidth = root.attrib["tilewidth"]
+            self.mTileHeight = root.attrib["tileheight"]
+            self.mMapWidth = root.attrib["width"]
+            self.mMapHeight = root.attrib["height"]
+            self.mNumTileColumns = 0
+            tilecount = 0
+            tilesetName = ""
             # find layers
             for child in root:  # <item> under map
                 if child.tag == "tileset":
-                    columns = child.attrib("columns")
-                    tilecount = child.attrib("tilecount")
-                    self.mTileOffsetX = child.attrib("spacing")
-                    self.mTileOffsetY = child.attrib("spacing")
-                    tilesetName = child[0][1].attrib("source")
+                    self.mNumTileColumns = int(child.attrib["columns"])
+                    tilecount = child.attrib["tilecount"]
+                    self.mTileOffsetX = child.attrib["spacing"]
+                    self.mTileOffsetY = child.attrib["spacing"]
+                    tilesetName = child[0].attrib["source"]
+                    print(tilesetName)
 
                 else: # if not <tileset> then we are <layer>
-                    self.mTileCodes.append(["layer_num", child.attrib("id")])
+                    self.mTileCodes.append(["layer_num", child.attrib["id"]])
                     idx = 0
                     tmp_array = []
+                    columns = int(child.attrib["width"])
                     for grandchild in child:  # tile number and render order
                         tiledata = grandchild.text
-                        tiledata.split(",")
+                        tiledata = tiledata.split(",")
+                        tmp_arr = []
+                        for sub in tiledata: # doing black magic to remove newline characters
+                            tmp_arr.append(re.sub('\n','',sub))
+                        tiledata = tmp_arr
                         for tilenum in tiledata: # store each row in a tmp list then append to mTileCodes
                             tmp_array.append(tilenum)
-                            if idx % columns == 0: # columns - 1 if tiled is base 1 not base 0
+                            if idx % columns == 0 and idx != 0: # if idx is divided equally by columns and idx != 0
                                 self.mTileCodes.append(tmp_array)
                                 tmp_array = []
                             idx += 1
-
-
+            print(self.mTileCodes)
 
         except FileNotFoundError:
             print("Couldnt open file '", fileName, "'", sep="")
