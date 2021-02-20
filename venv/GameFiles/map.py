5f5c2e39072e6076515978ed52165039f3436dcf
Lane <laneknauff@gmail.com>
2021-02-19 22:57:41 -0500

    Made small breakthroug in map class

--- venv/GameFiles/map.py
+++ venv/GameFiles/map.py
@@ -61,31 +61,29 @@ class Map:
                             #idx += 1
                         self.mTileCodes.append(tmp_array)
                         tmp_array = []
-            #print(self.mTileCodes)
+            # print(self.mTileCodes)
         except FileNotFoundError:
             print("Couldnt open file '", fileName, "'", sep="")
 
 
 
     def render(self, surf, cameraWorldPos, win_width, win_height):
+        #(128, 160) or 8x10 in tiles or 18 tileIDX
         winWidthInTiles = math.floor(win_width / self.mTileWidth)
         winHeightInTiles = math.floor(win_height / self.mTileHeight)
-        layerNumber = 0
-        cameraTileX = math.floor(cameraWorldPos[0] / self.mTileWidth)   # these twos vars are the xy worldspace
-        cameraTileY = math.floor(cameraWorldPos[1] / self.mTileHeight)  # coords of the tile at 0,0 screenspace
+
+        cameraTileX = math.floor(cameraWorldPos[0] / self.mTileWidth)
+        cameraTileY = math.floor(cameraWorldPos[1] / self.mTileHeight)
+
+        startTileIDX = cameraTileX+cameraTileY
+
         for i in range(winHeightInTiles):
-            #if i == 0 or i % self.mTilecount == 0:
-            #    layerNumber = int(self.mTileCodes[i][1])
-                #print(layerNumber)
-            #else:
-            #   self.mTileCodes[layerNumber]
             for j in range(winWidthInTiles):
-                # find tile pos in world space
+
+                # find tile pos in world space (check)
                 # convert world space to screen space
                 # blit to screen
                 pass
-
-        pass
 
 #   left to right first then go down one row until complete
 #   ------------------->
