88efae878c8cc9fc1cf0749288acfbd272db1bc9
roguespectator17 <78824261+roguespectator17@users.noreply.github.com>
2021-02-26 22:52:38 -0500

    Final Push
    I put my character class edits into the master branch, and made my finishing touches to everything else (got rid of redundant "player" module import).

--- venv/GameFiles/gameManager.py
+++ venv/GameFiles/gameManager.py
@@ -1,8 +1,8 @@
 import pygame
 
 import camera
-from characters import Characters
-from player import Player
+import characters
+
 from map import Map
 from camera import Camera
 
@@ -29,6 +29,8 @@ class GameManager:
         self.screen = pygame.display.set_mode((self.scr_w, self.scr_h))
         pygame.display.set_caption("Smol Game / Lab 5 / ETGG1802 / Prof: Jason / Team: Lane - Tanim - Evan | FPS: "
                                    + str(int(self.g_clock.get_fps())))
+        img1 = pygame.image.load("Sprites\\BATS.png")
+        img2 = pygame.image.load("Sprites\\Asteroid Brown.png")
         # background
         self.background = pygame.Surface(self.screen.get_size())
         self.background.fill(self.black)
@@ -40,12 +42,10 @@ class GameManager:
         self.font_type = pygame.font.get_default_font()
 
         # -- CLASS INSTANCES --
-        # character class instance
-        self.characters = Characters()
         # player class instance
-        self.player = Player(300, 300, ((self.scr_w / 2) - 32), ((self.scr_h / 2) - 32), 50, 32, img1, 1)
+        self.player = characters.Player(300, 300, ((self.scr_w / 2) - 32), ((self.scr_h / 2) - 32), 50, 32, img1, 1)
         # enemy class instance
-        self.enemy = Enemy(800, 800, 0, 0, 10, 160)
+        self.enemy = characters.Enemy(800, 800, 0, 0, 10, 160, img2)
         # map class instance
         self.map = Map("smol_map.tmx")
         # camera class instance
@@ -61,7 +61,7 @@ class GameManager:
             self.enemy.update(self.delta_time)
             self.player.update(self.delta_time)
             self.enemy.hit_detection(player.x, player.y, player.sprite)
-            self.player.draw()
+
             # - USER INPUT
             # event handling
             self.evt = pygame.event.poll()
@@ -72,6 +72,8 @@ class GameManager:
                     self.run = False
 
             # - DRAW
+            self.player.draw(map, map.getMapSize()[0], map.getMapSize()[1])
+            self.enemy.draw(map, map.getMapSize()[0], map.getMapSize()[1])
             # screen
             self.screen.blit(self.background, (0, 0))
             self.map.render(self.background, (0,0), self.scr_w, self.scr_h)
@@ -80,11 +82,11 @@ class GameManager:
             # text
 
             # camera
-<<<<<<< Updated upstream
+
             self.camera.update(self.player, self.player.sprite, self.player.sprite)
-=======
-            # self.camera.update(self.player, player.spritesize, player.spritesize)
->>>>>>> Stashed changes
+
+            self.camera.update(self.player, player.spritesize, player.spritesize)
+
 
             # - UPDATE DISPLAY
             pygame.display.flip()
